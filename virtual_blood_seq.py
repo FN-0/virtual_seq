84dc6a10abe19a9a4aa8fd985244bf4c49f8ec1d
Neidhardt <icefooooooooox@gmail.com>
2020-04-08 10:31:42 +0800

    update multithread

--- virtual_blood_seq.py
+++ virtual_blood_seq.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 import numpy as np
 import pandas as pd
 from random import randint
+from datetime import timedelta
 from random_vector import RandFloats, RandIntVec
+from multiprocessing.dummy import Pool as ThreadPool
 
 def read_data(filename, col=-1):
   df = pd.read_excel(filename, header=0)
@@ -54,18 +57,39 @@ def random_array(arr):
 def columns_name(col_name, n):
   return [("%s_%05d" % (col_name, n)) for n in range(n)]
 
+def get_columns(df):
+  columns_list = []
+  for col_name, col_data in df.iloc[:, 1:].iteritems():
+    col_list = [col_name, col_data]
+    columns_list.append(col_list)
+  return columns_list
+
+def random_df(seq_list, n):
+  col_name = seq_list[0]
+  col_data = seq_list[1]
+  data = col_data.to_numpy()
+  arr = random_n_partition(data, n)
+  randarr = random_array(arr)
+  return pd.DataFrame(randarr, columns=columns_name(str(col_name), n))
+
 def main():
   number2split = 5
   dataframe = read_data(sys.argv[1])
-  #vts = pd.DataFrame()
-  for col_name, col_data in dataframe.iloc[:, 1:].iteritems():
-    #print(columns_name(str(col_name), number2split))
-    data = col_data.to_numpy()
-    arr = random_n_partition(data, number2split)
-    randarr = random_array(arr)
-    df = pd.DataFrame(randarr, columns=columns_name(str(col_name), number2split))
-    dataframe = pd.concat([dataframe, df], axis=1)
-  dataframe.to_excel('virtual_seq.xlsx', index=False)
+  columns_list = get_columns(dataframe)
+  n_list = [number2split for _ in range(number2split)]
+
+  pool = ThreadPool(4)
+  results = pool.starmap(random_df, zip(columns_list, n_list))
+
+  # Close the pool and wait for the work to finish
+  pool.close()
+  pool.join()
+
+  results_df = pd.concat(results, axis=1)
+  results_df = pd.concat([dataframe, results_df], axis=1)
+  results_df.to_excel('virtual_seq.xlsx', index=False)
 
 if __name__ == "__main__":
+  start_time = time.time()
   main()
+  print("--- %s seconds ---" % str(timedelta(seconds=time.time()-start_time)))
